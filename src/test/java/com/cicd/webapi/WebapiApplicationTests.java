68d2d562351c2d276b64a27ccf0fb75c6e5526f1
Josuechc99 <josuechc.99@gmail.com>
2026-08-19 18:47:36 -0400

    Fixes

--- src/test/java/com/cicd/webapi/WebapiApplicationTests.java
+++ src/test/java/com/cicd/webapi/WebapiApplicationTests.java
@@ -41,7 +41,7 @@ class WebapiApplicationTests {
 				.accept(MediaType.TEXT_PLAIN))
 			.andExpect(status().isOk())
 			//.andExpect(content().string("Current Server Date: " + java.time.LocalDate.now()));
-			.andExpect(content().string("Current Server Date Time : " + java.time.LocalDateTime.now()));
+			.andExpect(content().string("Current Server Date Time: " + java.time.LocalDateTime.now()));
 	}
 
 }
